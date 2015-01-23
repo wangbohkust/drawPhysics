e4b8e88248d1a21dd47fe0fc524484a886cfbe15
wangbohkust <wangbo@wangbos-MacBook-Pro.local>
2015-01-23 12:02:02 -0800

    draw a object and turned into physics body

--- drawPhysics/GameScene.swift
+++ drawPhysics/GameScene.swift
@@ -9,37 +9,77 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    // currentPath keeps current drew path, currentDrawing keeps current shape
+    var currentPath = CGPathCreateMutable()
+    var currentDrawing = SKShapeNode()
+    let lineWidth : CGFloat = 4
+    
     override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
-        
-        self.addChild(myLabel)
+        setupScene()
+        setupGlobals()
+        setupGestureRecognizers()
     }
     
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
         /* Called when a touch begins */
-        
-        for touch: AnyObject in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
-        }
     }
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
     }
+    
+    func setupScene(){
+        self.backgroundColor = UIColor.whiteColor()
+        self.physicsWorld.gravity = CGVectorMake(0, -9.8)
+        self.physicsBody = SKPhysicsBody(edgeLoopFromRect: CGRect(x: 0, y: 0, width: self.size.width, height: self.size.height))
+    }
+    func setupGlobals(){
+        currentDrawing.strokeColor = UIColor.blackColor()
+        currentDrawing.lineWidth = lineWidth
+    }
+    func setupGestureRecognizers(){
+        //detects when a user is dragging their finger around the screen, corresponding action is below
+        self.view?.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: Selector("handlePan:")))
+    }
+    func handlePan(panReco:UIPanGestureRecognizer){
+        // current location point
+        let touchLoc = self.convertPointFromView(panReco.locationInView(panReco.view))
+        
+        if panReco.state == UIGestureRecognizerState.Began{
+            CGPathMoveToPoint(currentPath, nil, touchLoc.x, touchLoc.y)
+        }
+        else if panReco.state == UIGestureRecognizerState.Changed{
+            CGPathAddLineToPoint(currentPath, nil, touchLoc.x, touchLoc.y)
+            adjustDrawing()
+        }
+        else if panReco.state == UIGestureRecognizerState.Ended{
+            CGPathAddLineToPoint(currentPath, nil, touchLoc.x, touchLoc.y)
+            CGPathCloseSubpath(currentPath)
+            addObject()
+            
+            // build next new path
+            currentDrawing.removeFromParent()
+            currentPath = CGPathCreateMutable()
+        }
+    }
+    func adjustDrawing(){
+        currentDrawing.removeFromParent()
+        currentDrawing.path = currentPath
+        self.addChild(currentDrawing)
+    }
+    func addObject(){
+        let shapeNode = SKShapeNode(path: currentPath)
+       // shapeNode.strokeColor = getRandomColor()
+       // shapeNode.fillColor = getRandomColor()
+        shapeNode.lineWidth = lineWidth
+        self.addChild(shapeNode)
+        // change from shapenode to spritenode
+        let spriteNode = SKSpriteNode(texture: self.view?.textureFromNode(shapeNode), size: shapeNode.frame.size)
+        shapeNode.removeFromParent()
+        spriteNode.position = CGPoint(x: shapeNode.frame.width/2, y: shapeNode.frame.height/2)
+        spriteNode.physicsBody = SKPhysicsBody(texture: spriteNode.texture, alphaThreshold: 0.99, size: spriteNode.size)
+        self.addChild(spriteNode)
+    }
+    
+    
 }
